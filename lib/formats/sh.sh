d01eb54c4d0e1b0f0bc5d449fc11d883adf0f765
Ivan Shapovalov <intelfx@intelfx.name>
2023-03-16 00:10:10 +0400

    sh: cp_hardlink: preserve parent mtime
    Support `-k` flag in the hardlink method (preserve mtime of the parent
    directory).

--- lib/formats/sh.sh
+++ lib/formats/sh.sh
@@ -238,6 +238,9 @@ cp_hardlink() {
     printf "${COL_YELLOW}Hardlinking to original: ${COL_RESET}%%s\n" "$1"
     if original_check "$1" "$2"; then
         if [ -z "$DO_DRY_RUN" ]; then
+            if [ -n "$DO_KEEP_DIR_TIMESTAMPS" ]; then
+                touch -r "$(dirname "$1")" -- "$STAMPFILE"
+            fi
             # replace duplicate with hardlink
             mv -- "$1" "$1.temp"
             if ln "$2" "$1"; then
@@ -247,6 +250,10 @@ cp_hardlink() {
                 mv -- "$1.temp" "$1"
                 printf '%%s\n' "${COL_RED}^^^^^^ Error: could not hardlink.${COL_RESET}" >&2
                 return 1
+            fi
+            if [ -n "$DO_KEEP_DIR_TIMESTAMPS" ]; then
+                # restore parent mtime if we saved it
+                touch -r "$STAMPFILE" -- "$(dirname "$1")"
             fi
         fi
     fi
