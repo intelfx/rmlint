7fb3dc98d8087ab8f641308ab833978ce6ae1305
Ivan Shapovalov <intelfx@intelfx.name>
2025-04-30 21:36:20 +0700

    sh: do not choke on broken symlinks

--- lib/formats/sh.sh
+++ lib/formats/sh.sh
@@ -170,12 +170,12 @@ check_for_equality() {
 }
 
 original_check() {
-    if [ ! -e "$2" ]; then
+    if [ ! -e "$2" ] && [ ! -L "$2" ]; then
         printf '%%s\n' "${COL_RED}^^^^^^ Error: original has disappeared - cancelling...${COL_RESET}"
         return 1
     fi
 
-    if [ ! -e "$1" ]; then
+    if [ ! -e "$1" ] && [ ! -L "$1" ]; then
         printf '%%s\n' "${COL_RED}^^^^^^ Error: duplicate has disappeared - cancelling...${COL_RESET}"
         return 1
     fi
