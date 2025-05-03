b34724e3328491203726d3846a118c206e898b5d
Ivan Shapovalov <intelfx@intelfx.name>
2025-05-03 15:59:22 +0700

    (meta) SConstruct: cleanup non-broken checks
    Remove unnecessary line feeds and use `'\n'.join(...)` to pass multiline
    preambles for readability and consistency.

--- site_scons/rm_build_checks.py
+++ site_scons/rm_build_checks.py
@@ -273,7 +273,11 @@ def check_btrfs_h(context):
     rc = 1
     if tests.CheckHeader(
         context, 'linux/btrfs.h',
-        header='#include <stdlib.h>\n#include <sys/ioctl.h>'
+        header='\n'.join((
+            '#include <stdlib.h>',
+            '#include <sys/ioctl.h>',
+            '',
+        )),
     ):
         rc = 0
 
